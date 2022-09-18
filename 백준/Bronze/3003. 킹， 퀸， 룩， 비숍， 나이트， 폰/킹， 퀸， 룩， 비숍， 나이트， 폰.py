7a579199e04cc85f9f51d3195f9afcd7e5ee8119
kuno <101792115+rjs6418@users.noreply.github.com>
2022-09-18 23:16:13 +0900

    Update 킹， 퀸， 룩， 비숍， 나이트， 폰.py

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,5 +1,5 @@
 black = [1, 1, 2, 2, 2, 8]
 white = list(map(int, input().split()))
 
-changing = [str(black[i] - white[i]) for i in range(len(black)]
+changing = [str(black[i] - white[i]) for i in range(len(black))]
 print(' '.join(changing))
